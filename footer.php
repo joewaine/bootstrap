0776df5672aa0e53ac4ec45a3c50cfc52a197b37
Joe Waine <joseph@comradeagency.com>
2017-10-15 08:30:34 -0700

    bootstrap fix

--- footer.php
+++ footer.php
@@ -423,6 +423,14 @@ $('.mobile-menu').removeClass('expanded-menu');
 
   if(window.location.hash){
     $(window.location.hash).click();
+
+
+
+  $('html, body').animate({
+        scrollTop: $(".member-info-data").offset().top
+    }, 2000);
+
+
   }
 
 </script>
